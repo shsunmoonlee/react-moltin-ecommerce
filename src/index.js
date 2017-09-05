37d840bb10a782ebfb2a641d7b88e4d2f1387bf0
Lee, Seunghun <markandsh@gmail.com>
2017-09-05 13:21:39 +0800

    fixing deployment issues

--- src/index.js
+++ src/index.js
@@ -15,40 +15,15 @@ import Checkout from './components/Checkout';
 import Category from './components/Category';
 
 ReactDOM.render(
-  	<BrowserRouter onUpdate={() => window.scrollTo(0, 0)}>
+  	<BrowserRouter basename="/LeeArt-store/" onUpdate={() => window.scrollTo(0, 0)}>
       <Layout>
         <Switch>
       		<Route path="/" exact component={Home}/>
-      		<Route path="product/:id" component={ProductPage}/>
-      		<Route path="category/:id" component={Category}/>
-      		<Route path="checkout" component={Checkout}/>
+      		<Route path="/product/:id" component={ProductPage}/>
+      		<Route path="/category/:id" component={Category}/>
+      		<Route path="/checkout" component={Checkout}/>
     		</Switch>
       </Layout>
   	</BrowserRouter>,
   document.getElementById('root')
 );
-//
-// const routes = (
-// 	<Route path="/" component={Layout}>
-// 		<IndexRoute component={Home}/>
-// 		<Route path="product/:id" component={ProductPage}/>
-// 		<Route path="category/:id" component={Category}/>
-// 		<Route path="checkout" component={Checkout}/>
-// 	</Route>
-// );
-//
-// export default routes;
-//
-//
-//
-//
-// export default class AppRoutes extends React.Component {
-// 	render() {
-// 		return (
-// 			<BrowserRouter history={history}>
-//
-// 			</BrowserRouter>
-// 			<Router history={history} routes={routes} onUpdate={() => window.scrollTo(0, 0)}/>
-// 		);
-// 	}
-// }
